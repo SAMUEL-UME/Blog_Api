f4882d9ff6380b6f1d50863b312c7ee1f771c7bd
Samuel ume <cume357@gmail.com>
2022-11-06 15:10:12 +0100

    More changes

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const blogRoutes = require("./routes/blogRoutes");
 const cookieParser = require("cookie-parser");
 const { requireAuth, checkUser } = require("./middleware/authMiddleware");
 
-const {PORT} = require("./config/config")
+const { PORT } = require("./config/config");
 
 // Database connection
 Database.connect();
 
 const app = express();
-
 
 //midlleware
 app.use(express.json());
@@ -19,11 +18,13 @@ app.use(cookieParser());
 
 app.get("*", checkUser);
 app.get("/", (req, res) => {
-  res.json({ mssg: "welcome to my home page" });
+  res.json({
+    mssg: "Hey there, welcome to the best blogging plateform about the tech industry you'll find everywher",
+  });
 });
 // Settting up the routes
 app.use("/blogospot", authRoutes); //User routes
-app.use("/blogospot",blogRoutes); //Blog routes
+app.use("/blogospot", blogRoutes); //Blog routes
 
 //setting a 404 page
 app.get("*", function (req, res) {
